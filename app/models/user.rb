26cf25f05bf54896c0e995a3f25763d994e3663f
OlgaBieliaieva <belyaevaolya0@gmail.com>
2024-11-26 11:59:40 +0000

    Remove test model User

--- app/models/user.rb
+++
@@ -1,5 +0,0 @@
-class User < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 3, maximum: 20 }
-  validates :email, presence: true, uniqueness: { message: " is already registered" } # , format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :password, presence: true, length: { minimum: 8 }
-end
